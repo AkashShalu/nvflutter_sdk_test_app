cde567ee00c458bffca21ab2c539ef65b9aaca4d
Neeraj Sharma <34440781+AkashShalu@users.noreply.github.com>
2025-11-03 07:53:53 +0530

    2nd

---
+++ android/app/src/main/kotlin/com/notifyvisitors/nvflutter_sdk_test_app/MyApplication.kt
@@ -0,0 +1,18 @@
+package com.notifyvisitors.nvflutter_sdk_test_app
+
+import android.app.Application
+import com.flutter.notifyvisitors.NotifyvisitorsPlugin
+
+class MyApplication : Application() {
+
+    // NOTE: Replace the below with your own NOTIFYVISITORS_BRAND_ID & NOTIFYVISITORS_BRAND_ENCRYPTION_KEY
+    private val NOTIFYVISITORS_BRAND_ENCRYPTION_KEY: String = "DB52A5B00BB0D3BF426639A1B9FCF2F7"
+    private val NOTIFYVISITORS_BRAND_ID: Int = 7577
+
+    override fun onCreate() {
+        super.onCreate()
+
+        NotifyvisitorsPlugin.register(this, NOTIFYVISITORS_BRAND_ID, NOTIFYVISITORS_BRAND_ENCRYPTION_KEY);
+
+    }
+}
