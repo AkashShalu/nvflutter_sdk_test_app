5c78731638262da8b680161804f142dda1bf795d
Neeraj Sharma <34440781+AkashShalu@users.noreply.github.com>
2025-11-04 16:32:56 +0530

    Update MyApplication.kt

--- android/app/src/main/kotlin/com/notifyvisitors/nvflutter_sdk_test_app/MyApplication.kt
+++ android/app/src/main/kotlin/com/notifyvisitors/nvflutter_sdk_test_app/MyApplication.kt
@@ -7,7 +7,7 @@ class MyApplication : Application() {
 
     // NOTE: Replace the below with your own NOTIFYVISITORS_BRAND_ID & NOTIFYVISITORS_BRAND_ENCRYPTION_KEY
     private val NOTIFYVISITORS_BRAND_ENCRYPTION_KEY: String = "DB52A5B00BB0D3BF426639A1B9FCF2F7"
-    private val NOTIFYVISITORS_BRAND_ID: Int = 7577
+    private val NOTIFYVISITORS_BRAND_ID: Int = 8115
 
     override fun onCreate() {
         super.onCreate()
