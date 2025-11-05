db1c304ef0fbc80ee197d379bb1b25e174798dc8
Neeraj Sharma <34440781+AkashShalu@users.noreply.github.com>
2025-11-05 10:43:16 +0530

    Update MyApplication.kt

--- android/app/src/main/kotlin/com/notifyvisitors/nvflutter_sdk_test_app/MyApplication.kt
+++ android/app/src/main/kotlin/com/notifyvisitors/nvflutter_sdk_test_app/MyApplication.kt
@@ -6,7 +6,7 @@ import com.flutter.notifyvisitors.NotifyvisitorsPlugin
 class MyApplication : Application() {
 
     // NOTE: Replace the below with your own NOTIFYVISITORS_BRAND_ID & NOTIFYVISITORS_BRAND_ENCRYPTION_KEY
-    private val NOTIFYVISITORS_BRAND_ENCRYPTION_KEY: String = "DB52A5B00BB0D3BF426639A1B9FCF2F7"
+    private val NOTIFYVISITORS_BRAND_ENCRYPTION_KEY: String = "515CBDE82C402BDD85E4DFCCFD8904F6"
     private val NOTIFYVISITORS_BRAND_ID: Int = 8115
 
     override fun onCreate() {
